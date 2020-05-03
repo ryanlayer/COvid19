300eba17aefbc25703a8337b96fceb67ff1f9fc0
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2020-05-02 19:44:41 -0600

    made the line graph responsive to the scatter plots and vice versa

--- dash/app2.py
+++ dash/app2.py
@@ -7,14 +7,50 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
 import numpy as np
-
 import pandas as pd
+import uuid
+import time
+
+class MaxSizeCache:
+    def __init__(self, size):
+        self.cache = {}
+        self.size = size
+        self.birth_time = time.time()
+
+    def in_cache(self, key):
+        self.check_and_clear()
+        return key in self.cache.keys()
+
+    def add_to_cache(self, key, value):
+        # if the max size have been reached delete the first 5 keys
+        self.check_and_clear()
+        self.manage_size()
+        self.cache[key] = value
+
+    def get(self, key):
+        return self.cache[key]
+
+    def check_and_clear(self):
+        # check if the cache is older than 3 hours
+        if self.birth_time + 10800 < time.time():
+            print('Resenting Cache')
+            self.cache = {}
+            self.birth_time = time.time()
+
+    def manage_size(self):
+        if len(self.cache) == self.size:
+            print('Removing Some Cache Items')
+            keys = list(self.cache.keys())
+            for i in range(5):
+                del self.cache[keys[i]]
 
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 default_point_color = '#69A0CB'
+trend_session_cache = MaxSizeCache(100)
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 
 def get_map(lat, lon):
     mapbox_access_token = open(".mapbox_token").read()
@@ -56,56 +92,53 @@ def make_ws_hists(num_weeks, ws_df):
     return fig
 
 
+def move_index_to_end(items,index):
+    val = items[index]
+    del items[index]
+    items.append(val)
+    return items
+
+
+def move_all_index_to_end(X,Y,df_indexes,index):
+    X = move_index_to_end(X,index)
+    Y = move_index_to_end(Y,index)
+    df_indexes = move_index_to_end(df_indexes,index)
+    return X, Y, df_indexes
+
+
 @app.callback(
-    Output('map', 'figure'),
-    [Input('weekend_score', 'clickData'),
-    Input('slip_score', 'clickData')])
-def map_callback(ws_data,ss_data):
+    [Output('slip_score', 'figure'),
+    Output('weekend_score', 'figure'),
+    Output('trend_lines', 'figure'),
+    Output('map', 'figure')],
+    [Input('week-slider', 'value'),
+    Input('slip_score', 'clickData'),
+    Input('weekend_score', 'clickData'),
+    Input('trend_lines','clickData'),
+    Input('session-id','children')])
+def update_scatter_plots(selected_col_i,ss_data,ws_data,trend_data,session_id):
     lon = 40.588928
     lat = -112.071533
 
     ctx = dash.callback_context
-
-    if not ctx.triggered:
-        button_id = 'No clicks yet'
-    elif ctx.triggered[0]['prop_id'] == 'weekend_score.clickData':
+    index = -1
+    if ctx.triggered[0]['prop_id'] == 'weekend_score.clickData':
+        index = int(ws_data['points'][0]['customdata'])
         lon = ws_df.iloc[int(ws_data['points'][0]['customdata']),:]['lon']
         lat = ws_df.iloc[int(ws_data['points'][0]['customdata']),:]['lat']
-    else:
+    elif ctx.triggered[0]['prop_id'] == 'slip_score.clickData':
+        index = int(ss_data['points'][0]['customdata'])
         lon = ss_df.iloc[int(ss_data['points'][0]['customdata']),:]['lon']
         lat = ss_df.iloc[int(ss_data['points'][0]['customdata']),:]['lat']
+    elif ctx.triggered[0]['prop_id'] == 'trend_lines.clickData':
+        trend_index = trend_data['points'][0]['curveNumber']
+        index = trend_session_cache.get(session_id)[trend_index]
+        lon = trend_df.iloc[index,:]['lon']
+        lat = trend_df.iloc[index,:]['lat']
 
-    return get_map(lon, lat)
-
-def move_index_to_end(X,Y,df_indexes,index):
-    x_val = X[index]
-    y_val = Y[index]
-    df_index_val = df_indexes[index]
-    del X[index]
-    del Y[index]
-    del df_indexes[index]
-    X.append(x_val)
-    Y.append(y_val)
-    df_indexes.append(df_index_val)
-    return X, Y, df_indexes
-
-@app.callback(
-    [Output('slip_score', 'figure'),
-    Output('weekend_score', 'figure')],
-    [Input('week-slider', 'value'),
-    Input('slip_score', 'clickData'),
-    Input('weekend_score', 'clickData')])
-def update_scatter_plots(selected_col_i,ss_data,ws_data):
-    ctx = dash.callback_context
-
-    point_index = -1
-    if ctx.triggered[0]['prop_id'] == 'weekend_score.clickData':
-        point_index = int(ws_data['points'][0]['customdata'])
-    elif ctx.triggered[0]['prop_id'] == 'slip_score.clickData':
-        point_index = int(ss_data['points'][0]['customdata'])
-
-    return slip_score_callback(selected_col_i,ss_data,point_index),  \
-           weekend_score_callback(selected_col_i,ws_data,point_index)
+    return slip_score_callback(selected_col_i,ss_data,index),  \
+           weekend_score_callback(selected_col_i,ws_data,index), \
+           make_trend(index,session_id), get_map(lon,lat)
 
 
 def slip_score_callback(selected_col_i,ss_data,point_index):
@@ -122,7 +155,7 @@ def slip_score_callback(selected_col_i,ss_data,point_index):
     if point_index != -1:
         point_color = 'red'
         # move the point of interest to the end so it displays on top
-        X,Y,df_indexes = move_index_to_end(X,Y,df_indexes,point_index)
+        X,Y,df_indexes = move_all_index_to_end(X,Y,df_indexes,point_index)
 
     marker_colors = [default_point_color] * len(X)
     marker_colors[-1] = point_color
@@ -149,6 +182,7 @@ def slip_score_callback(selected_col_i,ss_data,point_index):
         )
     }
 
+
 def weekend_score_callback(selected_col_i,ws_data,point_index):
     ws_weeks = ws_df.columns[6:]
     selected_col = ws_weeks[selected_col_i]
@@ -160,7 +194,7 @@ def weekend_score_callback(selected_col_i,ws_data,point_index):
     if point_index != -1:
         point_color = 'red'
         # move the point of interest to the end so it displays on top
-        X,Y,df_indexes = move_index_to_end(X,Y,df_indexes,point_index)
+        X,Y,df_indexes = move_all_index_to_end(X,Y,df_indexes,point_index)
 
     traces = []
     marker_colors = [default_point_color] * len(ws_df[selected_col])
@@ -187,6 +221,7 @@ def weekend_score_callback(selected_col_i,ws_data,point_index):
         )
     }
 
+
 @app.callback(
     Output('weekend_hist', 'figure'),
     [Input('week-slider', 'value')])
@@ -196,7 +231,7 @@ def make_ws_hist(selected_col_i):
     return { 'data' : [{ 'x' :ws_df[selected_col], 'type': 'histogram' } ]}
 
 
-def make_trend():
+def make_trend(index,session_id):
     dow_date_time= [ x.split()[1:] for x in trend_df.columns[6:]]
     date_time = [x[1:] for x in dow_date_time]
 
@@ -204,20 +239,29 @@ def make_trend():
     b_norm = 1 + (5*((b - np.min(b)) / np.max(b)))
 
     fig = go.Figure()
-
+    traces = []
+    indexes = list(range(trend_df.shape[0]))
     for idx,row in trend_df.iterrows():
+        line_color = default_point_color
+        if idx == index:
+            line_color = 'red'
         y = row.tolist()[6:]
         x = [i for i in range(len(y))]
         loc = str(row.lat) + ',' + str(row.lon)
-        fig.add_trace(go.Scatter(x=x,
+        traces.append(go.Scatter(x=x,
                                  y=y,
                                  text=loc,
                                  opacity=0.5,
                                  line=dict(width=b_norm[idx],
-                                           color=default_point_color)))
+                                           color=line_color)))
         if idx == 100:
             break
-
+    traces = move_index_to_end(traces,index)
+    trace_indexes = list(range(len(traces)))
+    trace_indexes = move_index_to_end(trace_indexes,index)
+    trend_session_cache.add_to_cache(session_id,trace_indexes)
+    for t in traces:
+        fig.add_trace(t)
     fig.update_layout(showlegend=False)
     return fig
 
@@ -237,30 +281,36 @@ hists = make_ws_hists(num_weeks, ws_df)
 
 marks={i:pretty_weeks[i] for i in range(num_weeks)}
 
-app.layout = html.Div([
-    dbc.Col([
-        dbc.Row( [
-            dbc.Col( dcc.Graph(id='map',
-                               figure=get_map(40.588928, -112.071533)))
+def layout():
+    session_id = str(uuid.uuid4())
+    return html.Div([
+        dbc.Col([
+            dbc.Row( [
+                dbc.Col( dcc.Graph(id='map',
+                                   figure=get_map(40.588928, -112.071533)))
+            ]),
+            dbc.Row([
+                dbc.Col(dcc.Graph(id='trend_lines'))
+            ]),
+            dbc.Row([
+                dbc.Col(dcc.Graph(id='weekend_hist'), width=4),
+                dbc.Col(dcc.Graph(id='weekend_score'), width=4),
+                dbc.Col(dcc.Graph(id='slip_score'), width=4)
+            ],no_gutters=True,),
+            dbc.Row( [
+                dbc.Col(dcc.Slider(id='week-slider',
+                                   min=0,
+                                   max=num_weeks-1,
+                                   value=num_weeks-1,
+                                   marks=marks,
+                                   step=None)),
+            ]),
         ]),
-        dbc.Row([
-            dbc.Col(dcc.Graph(figure=make_trend()))
-        ]),
-        dbc.Row([
-            dbc.Col(dcc.Graph(id='weekend_hist'), width=4),
-            dbc.Col(dcc.Graph(id='weekend_score'), width=4),
-            dbc.Col(dcc.Graph(id='slip_score'), width=4)
-        ],no_gutters=True,),
-        dbc.Row( [
-            dbc.Col(dcc.Slider(id='week-slider',
-                               min=0,
-                               max=num_weeks-1,
-                               value=num_weeks-1,
-                               marks=marks,
-                               step=None)),
-        ]),
+        # Hidden div inside the app that stores the intermediate value
+        html.Div(session_id,id='session-id', style={'display': 'none'})
     ])
-])
+
+app.layout = layout
 
 
 if __name__ == '__main__':
